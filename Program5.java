7433f612017ccf315c1429881ea96ba47d2b3a4a
kcarchin <kcarchin@mtu.edu>
2015-12-09 13:31:08 -0500

    Test Change 1

--- Program5.java
+++ Program5.java
@@ -121,6 +121,7 @@ public class Program5 extends Application {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		launch(args);
+		//launch(args);
+		System.out.println("Test.");
 	}
 }
